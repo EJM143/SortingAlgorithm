9a732270627e1a2c17e39020801c2b207441acf1
Rory Hackney <72114389+roryhackney@users.noreply.github.com>
2024-01-14 16:14:16 -0800

    Project folder structure
    Adding Git Screenshots folder (for command line Git screenshots requirement)
    src - for source code, so we don't have to make this manually for IntelliJ etc
    Tests - for JUnit tests, may need to mark as Test Sources Root manually

---
+++ Tests/testTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class testTest {
+    @Test
+    public void test_test() {
+        assertEquals(1, 3 - 2);
+    }
+
+}
