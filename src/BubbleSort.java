ad5188fe4bc3eacad4751cfcb408a5117f7c7678
Kelvin R <kelvinrbNsc@gmail.com>
2024-01-24 16:32:07 -0800

    intial commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,40 @@
+/**
+ * Class implementation to sort an array of integers using BubbleSort
+ *
+ * @author KRB
+ * @version 1.1
+ */
+public class BubbleSort implements SortingAlgoInterface{
+    /**
+     * Method to sort a given array of integers
+     * @param array Desired array of integers to sort
+     */
+    @Override
+    public void sort(int[] array) {
+        if (array.length == 0) {
+            System.out.println("No elements in the array to sort!");
+            return;
+        }
+
+        int size = array.length;
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i; j++) {
+                if (array[j + 1] < array[j]) {
+                    int helper = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = helper;
+                }
+            }
+        }
+    }
+
+    /**
+     * Method to return the name of the sorting algorithm
+     * @return String name
+     */
+    @Override
+    public String getName() {
+        return "BubbleSort";
+    }
+}
---
+++ src/ShellSort.java
@@ -0,0 +1,41 @@
+/**
+ * Class implementation to sort an array of integers using ShellSort
+ *
+ * @author KRB
+ * @version 1.1
+ */
+public class ShellSort implements SortingAlgoInterface{
+    /**
+     * Method to sort a given array of integers
+     * @param array Desired array of integers to sort
+     */
+    @Override
+    public void sort(int[] array) {
+        if (array.length == 0) {
+            System.out.println("No elements in the array to sort!");
+            return;
+        }
+
+        int size = array.length;
+
+        for (int distance = size/2; 0 < distance; distance = distance/2) {
+            for (int i = distance; i < size; i++) {
+                int helper = array[i];
+                int j = -1;
+                for (j = i; distance <= j && helper < array[j - distance]; j = j - distance) {
+                    array[j] = array[j - distance];
+                }
+                array[j] = helper;
+            }
+        }
+    }
+
+    /**
+     * Method to return the name of the sorting algorithm
+     * @return String name
+     */
+    @Override
+    public String getName() {
+        return "ShellSort";
+    }
+}
